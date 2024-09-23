faf16347feef2f8e08922e413a7abde23beebd5c
eliza W <eliza@elizas-MacBook-Air.local>
2024-09-23 13:44:40 +0930

    formatting on customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,22 +1,17 @@
+#include "Customer.h"
+#include "Person.h"
+
 #include <iostream>
 #include <string>
-#include "Person.h"
-#include "Customer.h"
 
-int Customer::getID(){
-    return ID;
+int Customer::getID() { return ID; }
+
+void Customer::setID(int ID) { this->ID = ID; }
+
+bool Customer::getCorrectOrNot() { return correctOrNot; }
+
+void Customer::setCorrectOrNot(bool correctOrNot) {
+  this->correctOrNot = correctOrNot;
 }
 
-void Customer::setID(int ID){
-    this->ID = ID;
-}
-
-bool Customer::getCorrectOrNot(){
-    return correctOrNot;
-}
-
-void Customer::setCorrectOrNot(bool correctOrNot){
-    this->correctOrNot = correctOrNot;
-}
-
-void checker(){}
+void checker() {}
