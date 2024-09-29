f88451c5362acce81ac89f7f3d156e057a715dce
Mikayla Brierley <mikaylabrierley@Mikaylas-MacBook-Pro-3.local>
2024-09-29 11:57:40 +0930

    testing file for main game created

---
+++ main_test.cpp
@@ -0,0 +1,14 @@
+#include "Person.h"
+#include "Player.h"
+#include "Customer.h"
+#include "Patient.h"
+#include "Police.h"
+#include "Game.h"
+#include "Medicine.h"
+#include "Storage.h"
+#include "EndScene.h"
+
+int main(){
+
+    
+}
