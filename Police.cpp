176a7a23db99070ce41939bbc9407868c85f90a9
SSuhani <a1923326@adelaide.edu.au>
2024-10-08 20:12:51 +1030

    Update the Police.cpp

--- Police.cpp
+++ Police.cpp
@@ -3,5 +3,14 @@
 #include "Person.h"
 #include "Customer.h"
 #include "Police.h"
+using namespace std;
+void Police::arrest(){
+    cout << "Stop right there, you’ve been arrested for selling medication without a proper license!" << endl;
+}
 
-void Police::arrest(){}
+void Police::rightDialogue(){
+    cout<<" hmm okay, have a good day"<<endl;
+}
+void Police::wrongDialogue(){
+    cout<<" This isn't right"<<endl;
+}
