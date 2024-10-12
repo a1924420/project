db63c7721df5028a7d97c9954e0e6ec54c17cf3d
SSuhani <a1923326@adelaide.edu.au>
2024-10-13 10:18:25 +1030

    Create CustomerTest.h

---
+++ CustomerTest.h
@@ -0,0 +1,21 @@
+#ifndef CUSTOMERTEST_H
+#define CUSTOMERTEST_H
+
+#include <iostream>
+#include <string>
+#include "Person.h"
+#include "Medicine.h"
+#include "Player.h"
+
+
+class CustomerTest{
+private:
+int ID;
+    bool correctOrNot;
+    std::string illness;
+    std::string medicalHistory;
+public:
+
+
+};
+#endif
