f68c13a2582637ea5d9f610bcd3971f8562fcd4a
SSuhani <a1923326@adelaide.edu.au>
2024-10-15 14:38:29 +1030

    Update Police.h comments

--- Police.h
+++ Police.h
@@ -12,13 +12,15 @@ class Police: public Customer{
     private:
 
     public:
-
+// Default constructor
     Police();
-
+// Parameterised constructor
     Police(std::string name, int age, int ID, std::string illness, std::string medicalHistory);
-
+// Member function for the dialogue when the player denies the police officer the medication 
     void rightDialogue();
+// Member function for the dialogue when the player gives the medication to the police officer
     void wrongDialogue();
+// Funtion for when the player is arrested for giving the the medication to the police officer
     static void arrest();
 
 };
