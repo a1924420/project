a3912bd5cbe908e3ba93e2556071d6ccf627ceaf
SSuhani <a1923326@adelaide.edu.au>
2024-10-15 14:38:15 +1030

    Police.cpp comments

--- Police.cpp
+++ Police.cpp
@@ -5,14 +5,14 @@
 #include "Police.h"
 
 using namespace std;
-
+// Initailaises the police officer
 Police::Police(){
     setName(" ");
     setAge(0);
     setID(0);
     setCorrectOrNot(false);
 }
-
+// Initailaises the police officer
 Police::Police(std::string name, int age, int ID, std::string illness, std::string medicalHistory){
     setName(name);
     setAge(age);
@@ -21,15 +21,16 @@ Police::Police(std::string name, int age, int ID, std::string illness, std::stri
     setMedicalHistory(medicalHistory);
 
 }
-
+// Arrests the player 
 void Police::arrest(){
     cout << "Stop right there, you’ve been arrested for selling medication without a proper license!" << endl;
     exit(0); // ends game at this point - ends entire code run
 }
-
+//This is said by the police officer when the player denies the police.
 void Police::rightDialogue(){
     cout << "Hmm okay, have a good day." << endl;
 }
+// This is the wrong dialogue that will be output when the police catch the player
 void Police::wrongDialogue(){
     cout << "Wait, so you do sell medicine here?" << endl;
     cout << "That's illegal!" << endl;
