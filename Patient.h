046ca13fa6ecb748aae427d92ff300a359c6a832
SSuhani <a1923326@adelaide.edu.au>
2024-10-15 14:38:24 +1030

    Update Patient.h comments

--- Patient.h
+++ Patient.h
@@ -13,16 +13,18 @@ class Patient: public Customer{
     int prescriptionID;
 
     public:
-
+// default constructor
     Patient();
-
+// Parameterised constructor
     Patient(std::string name, int age, int ID, std::string illness, std::string medicalHistory, int prescriptionID);
-
+//Getter for the prescription ID for the patient
     int getPrescriptionID();
-
+// Setter for the prescription ID
     void setPrescriptionID(int prescriptionID);
-
+// Member function for the dialogue when the player gives the correct medication to the customer 
     void rightDialogue();
+
+// Member function for the dialogue when the player gives the wrong medication to the customer 
     void wrongDialogue();
 
 };
